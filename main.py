aec9a9a39916bde09ca1b7ba91e6a44271412c38
Arctican4Real <adeebsr700@gmail.com>
2024-12-05 22:26:13 +0900

    Add boilerplate for main code

---
+++ main.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(
+        username = "root",
+        password = "root",
+        host = "localhost",
+        database = "kitcritic"
+    )
+curs = cnx.cursor()
+
+# Function to find the total score as per catagory
+
+# Function to retrieve data (NAZIA)
+
+# Function to update data (NAZIA)
+
+# Function to give overall score to kit
