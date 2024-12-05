097a8b191131b65bfd781bb12b505ad1367bf455
Neil <neilshri007@gmail.com>
2024-12-06 01:25:56 +0900

    worked on the window manager

--- GUI.py
+++ GUI.py
@@ -5,79 +5,97 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 
 KV = '''
-MDScreen:
-    md_bg_color: self.theme_cls.backgroundColor
-    MDLabel:
-        text: "Enter Duration(Days)"
-        pos_hint: {"center_x": .75, "center_y": .69}
-    MDLabel:
-        text: "Enter Max People"
-        pos_hint: {"center_x": .75, "center_y": .47}
-        
-    MDBoxLayout:
-        orientation: "vertical"
-        spacing: "80dp"
-        adaptive_height: True
-        size_hint_x: .5
-        pos_hint: {"center_x": .5, "center_y": .6}
-      
-        MDSlider:
-            id: duration_slider
+Window_Manager:
+    first_window:
+    second_window:
+<first_window>:
+    name: "first"
+    MDScreen:
+        md_bg_color: self.theme_cls.backgroundColor
+        MDLabel:
+            text: "Enter Duration(Days)"
+            pos_hint: {"center_x": .75, "center_y": .69}
+        MDLabel:
+            text: "Enter Max People"
+            pos_hint: {"center_x": .75, "center_y": .47}
+    
+        MDBoxLayout:
+            orientation: "vertical"
+            spacing: "80dp"
+            adaptive_height: True
+            size_hint_x: .5
+            pos_hint: {"center_x": .5, "center_y": .6}
+    
+            MDSlider:
+                id: duration_slider
+    
+                step: 1
+                max: 30
+                min:1
+                value: 1
+    
+                MDSliderHandle:
+    
+                MDSliderValueLabel:
+        MDBoxLayout:
+            orientation: "vertical"
+            spacing: "80dp"
+            adaptive_height: True
+            size_hint_x: .5
+            pos_hint: {"center_x": .5, "center_y": .4}
+    
+            MDSlider:
+                id: people_slider
+    
+                step: 1
+                max: 15
+                min:1
+                value: 1
+    
+                MDSliderHandle:
+    
+                MDSliderValueLabel:
+    
+    
 
-            step: 1
-            max: 30
-            min:1
-            value: 1
 
-            MDSliderHandle:
 
-            MDSliderValueLabel:
-    MDBoxLayout:
-        orientation: "vertical"
-        spacing: "80dp"
-        adaptive_height: True
-        size_hint_x: .5
-        pos_hint: {"center_x": .5, "center_y": .4}
-      
-        MDSlider:
-            id: people_slider
-
-            step: 1
-            max: 15
-            min:1
-            value: 1
-
-            MDSliderHandle:
-
-            MDSliderValueLabel:
-                
-                
-
-                    
-                
 
     MDButton:
         style: "outlined"
         pos_hint: {"center_x": .3, "center_y": .26}
+        on_release: app.root.current = "second"
 
         MDButtonText:
             text: "Back"
-            
+
     MDButton:
         style: "filled"
         pos_hint: {"center_x": .42, "center_y": .26}
         on_press: app.save_values()
         MDButtonText:
             text: "Enter"
-            
 
+
+    <second_window>:
+    name: "second"
+    MDScreen:
+        md_bg_color: self.theme_cls.backgroundColor
+
+        MDButton:
+            style: "filled"
+            pos_hint: {"center_x": .5, "center_y": .5}
+            on_release: app.root.current = "first"
+            MDButtonText:
+                text: "Goback"
+                
 '''
 
-class firstwindow(MDScreen):
+class first_window(MDScreen):
     pass
-class secondwindow(MDScreen):
+class second_window(MDScreen):
     pass
-class windowmanager(MDScreenManager):
+class Window_Manager(MDScreenManager):
     pass
 
 
