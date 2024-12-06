e08b0210b24877f5015602b23e5957749b85fc85
Neil <neilshri007@gmail.com>
2024-12-06 19:12:06 +0900

    changed the location of the front page to be the first page,removed unused imports and made some minor cleanup

--- GUI.py
+++ GUI.py
@@ -1,14 +1,26 @@
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
-from kivymd.uix.screenmanager import MDScreenManager
-from kivymd.uix.screen import MDScreen
 
 
 KV = '''
 MDScreenManager:
+
     MDScreen:
-        name: "screen A"
+        name: "front_page"
+        md_bg_color: "cadetblue"
+        
+        MDButton:
+            pos_hint: {"center_x": .5,"center_y": .5}
+            on_release:
+                root.current = "first_input_page"
+
+            MDButtonText:
+                text: "Start"
+                
+                
+    MDScreen:
+        name: "first_input_page"
         md_bg_color: self.theme_cls.backgroundColor
         MDLabel:
             text: "Enter Duration(Days)"
@@ -53,45 +65,26 @@ MDScreenManager:
                 MDSliderHandle:
     
                 MDSliderValueLabel:
-                    
-                    
-    
-                        
-                    
     
         MDButton:
             style: "outlined"
             pos_hint: {"center_x": .3, "center_y": .26}
             on_release:
-                root.current = "screen B"
+                root.current = "front_page"
             MDButtonText:
                 text: "Back"
-            
-                
+                   
         MDButton:
             style: "filled"
             pos_hint: {"center_x": .42, "center_y": .26}
             on_press: app.save_values()
             MDButtonText:
                 text: "Enter"
-    MDScreen:
-        name: "screen B"
-        md_bg_color: "cadetblue"
-        
-        MDButton:
-            pos_hint: {"center_x": .5,"center_y": .5}
-            on_release:
-                root.current = "screen A"
-
-            MDButtonText:
-                text: "Start"
 
 '''
 
 
 class Example(MDApp):
-
-
     def save_values(self):
         duration = self.root.ids.duration_slider.value
         max_people = self.root.ids.people_slider.value
@@ -105,8 +98,6 @@ class Example(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Blue"
         return Builder.load_string(KV)
-
-
 
 
 Example().run()
