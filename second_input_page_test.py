2e1493244c7eeb43906e4e61bfeb87f30971c073
Neil <neilshri007@gmail.com>
2024-12-07 23:03:04 +0900

    very basic list item adding from an input field completed. must test it further

--- second_input_page_test.py
+++ second_input_page_test.py
@@ -28,6 +28,7 @@ MDScreen:
                 adaptive_height: True
 
             MDTextField:
+                id: inputfield
                 mode: "outlined"
                 size_hint_x: None
                 width: "240dp"
@@ -35,6 +36,7 @@ MDScreen:
             MDButton:
                 style: "outlined"
                 pos_hint: {"center_x": .60, "center_y": .26}
+                on_release:app.on_start()
                 MDButtonText:
                     text: "Back"
 
@@ -51,9 +53,14 @@ class Example(MDApp):
         self.theme_cls.theme_style = "Dark"
         return Builder.load_string(MAIN_KV)
 
-    def on_start(self):
+    Window.size = [dp(350), dp(600)]
 
-        info = ["ff","553554"]
+    def on_start(self):
+        info = []
+
+        user_input = self.root.ids.inputfield.text
+        info.append(user_input)
+
         for info_item in info:
             self.root.ids.main_scroll.add_widget(
                 MDListItem(
@@ -64,7 +71,7 @@ class Example(MDApp):
                 )
             )
 
-        Window.size = [dp(350), dp(600)]
+
 
 
 Example().run()
