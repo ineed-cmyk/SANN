6d519923bcccd342c9799233726e56fc477ecdae
Neil <neilshri007@gmail.com>
2024-12-07 22:31:15 +0900

    created a test page to test out the scrollview for the next input page. to be deleted later

---
+++ second_input_page_test.py
@@ -0,0 +1,64 @@
+
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.metrics import dp
+
+from kivymd.app import MDApp
+from kivymd.uix.list import (
+    MDListItem,
+    MDListItemHeadlineText,
+    MDListItemSupportingText,
+    MDListItemLeadingIcon,
+)
+
+
+MAIN_KV = '''
+MDScreen:
+    md_bg_color: app.theme_cls.backgroundColor
+
+    MDScrollView:
+        do_scroll_x: False
+
+        MDBoxLayout:
+            id: main_scroll
+            orientation: "vertical"
+            adaptive_height: True
+
+            MDBoxLayout:
+                adaptive_height: True
+
+                MDLabel:
+                    theme_font_size: "Custom"
+                    text: "OS Info"
+                    font_size: "55sp"
+                    adaptive_height: True
+                    padding: "10dp", "20dp", 0, 0
+
+                MDIconButton:
+                    icon: "menu"
+                    pos_hint: {"center_y": .5}
+'''
+
+
+class Example(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(MAIN_KV)
+
+    def on_start(self):
+
+        info = ["ff","553554"]
+        for info_item in info:
+            self.root.ids.main_scroll.add_widget(
+                MDListItem(
+                    MDListItemHeadlineText(
+                        text=info_item,
+                    ),
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+            )
+
+        Window.size = [dp(350), dp(600)]
+
+
+Example().run()
